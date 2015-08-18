125fb70fb205712ece56983667592f9335badbbe
nitzan <nitzan@simplee.com>
2015-08-18 13:08:32 +0300

    Associated and nested topics and posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,46 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-    authorize @posts
-  end
 
   def show
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize @post
   end
 
   def create
+    @topic = Topic.find(params[:topic_id])    
     @post = current_user.posts.new(params.require(:post).permit(:title, :body))
+    @post.topic = @topic
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic,@post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
-      render :new;
+      render :new
     end
   end
 
   def edit
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
     authorize @post
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title,:body))
       flash[:notice] = "post was updated"
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
-      render :edit
+      render :new
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
 
-  get 'welcome/contact'
 
   root to: 'welcome#index'
 end
