0f0c9a93008c24869a9c175c9a65da3bb7e4b24e
nitzan <nitzan@simplee.com>
2015-08-19 12:00:51 +0300

    adding markdown support functionality

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'faker'
 gem 'devise'
 gem 'figaro'
 gem 'pundit'
+gem 'redcarpet'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,20 @@
 module ApplicationHelper
-  def form_group_tag(errors, &block)
-    if errors.any?
-      content_tag :div, capture(&block), class: 'form-group hass-error'
-    else  
-      content_tag :div, capture(&block), class: 'form-group'
-    end
+def my_name
+  "nitzan otorgust"
+end 
+
+def form_group_tag(errors, &block)
+  if errors.any?
+    content_tag :div, capture(&block), class: 'form-group hass-error'
+  else  
+    content_tag :div, capture(&block), class: 'form-group'
   end
+end
+
+def markdown(text)
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {fenced_code_blocks: true}
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render text).html_safe
+end
 end
