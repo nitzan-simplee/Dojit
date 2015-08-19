de257d1ded8f07abdb2983cc4135ee4948daf4d0
nitzan <nitzan@simplee.com>
2015-08-19 12:51:46 +0300

    moving markdown functionality to post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  include ApplicationHelper
   has_many :comments
   belongs_to :user
   belongs_to :topic
@@ -9,4 +10,16 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+  def markdown_title
+    render_as_markdown title
+  end
+
+  def markdown_body
+    render_as_markdown body
+  end
+
+  private
+  def render_as_markdown(text)
+    markdown text
+  end
 end
