36eb0e0ee4812c31714e79c019149e8bd4aefbe0
nitzan <nitzan@simplee.com>
2015-08-23 14:42:54 +0300

    Adding support to aws avatar upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :avatar) }
   end
 end
