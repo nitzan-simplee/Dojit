ce459d0ce0e50f5e408b302e4e0b32ba936a2ba6
nitzan <nitzan@simplee.com>
2015-08-26 19:48:40 +0300

    adding specs for the profile assigment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -5,11 +5,10 @@ describe "Visiting profiles" do
   include TestFactories
 
   before do
-    @user = authenticated_user
-    @post = associated_post(user: @user)
-    @comment = Comment.new(user:@user,  body: "A comment")
-    allow(@comment).to receive(:send_favorite_emails)
-    @comment.save
+     @user = authenticated_user
+     @post = associated_post(user: @user)
+      @comment = comment_without_email(user: @user, 
+        post: @post)
   end
 
   describe "not signed in" do
@@ -23,5 +22,24 @@ describe "Visiting profiles" do
       expect( page ).to have_content(@comment.body)
     end
   end
+
+  describe "user does sign in" do
+    before do
+      login_as(@user, :scope => :user)
+    end
+
+    after do
+      Warden.test_reset! 
+    end
+
+   it "shows profile" do
+    visit user_path(@user)
+    expect(current_path).to eq(user_path(@user))
+
+    expect( page ).to have_content(@user.name)
+    expect( page ).to have_content(@post.title)
+    expect( page ).to have_content(@comment.body)
+  end
+end
 
 end
--- spec/support/test_factories.rb
+++ spec/support/test_factories.rb
@@ -1,4 +1,7 @@
 module TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
+  
   def associated_post(options={})
   post_options = {title: 'post title',
     body: 'post bodies must be pretty long',
@@ -7,11 +10,19 @@ module TestFactories
   Post.create(post_options)
   end
 
-def authenticated_user(options={})
-  user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
-  user = User.new(user_options)
-  user.skip_confirmation!
-  user.save
-  user
-end
+  def authenticated_user(options={})
+    user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
+    user = User.new(user_options)
+    user.skip_confirmation!
+    user.save
+    user
+  end
+
+  def comment_without_email(options={})
+    comment_options = {body: "this is the comment body"}.merge(options)
+    comment = Comment.new(comment_options)
+    allow(comment).to receive(:send_favorite_emails)
+    comment.save
+    comment
+  end
 end
