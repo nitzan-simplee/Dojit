9e99b4be5fe603875cb4bd8adb8fba03ca2be24c
nitzan <nitzan@simplee.com>
2015-08-27 19:26:02 +0300

    adding ajax rendering to creating comments as well

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,17 +20,22 @@ end
 
 def create
   @post = Post.find(params[:post_id])
-  @topic = @post.topic    
-  @comment = @post.comments.build(comment_params)
-  @comment.user = current_user
+  @comments = @post.comments
+
+  @comment = current_user.comments.build( comment_params )
+  @comment.post = @post
+  @new_comment = Comment.new
+
   authorize @comment
+
   if @comment.save
-    flash[:notice] = "Comment was saved."
-    redirect_to [@topic,@post]
+    flash[:notice] = "Comment was created."
   else
     flash[:error] = "There was an error saving the comment. Please try again."
-    @comments = @post.reload.comments
-    render 'posts/show'
+  end
+
+  respond_with(@comment) do |format|
+    format.html { redirect_to [@post.topic, @post] }
   end
 end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
-    validates :body, length: {minimum: 5}, presence: true
-    validates :user, presence: true
+  validates :body, length: {minimum: 5}, presence: true
+  validates :user, presence: true
 
-    after_create :send_favorite_emails
+  default_scope { order('updated_at DESC') }
+
+  after_create :send_favorite_emails
 
     private
     def send_favorite_emails
